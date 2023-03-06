e769a72066de4067984efd9fcb0e78f228f97931
nsunklodas <121260132+nsunklodas@users.noreply.github.com>
2023-03-06 03:00:19 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "flex";
+    evt.currentTarget.className += " active";
+  }
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  const hamburger = document.querySelector(".hamburger");
+  const links = document.querySelector("#links");
+
+  hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    links.classList.toggle("active");
+  })
+
+  document.querySelectorAll("a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    links.classList.remove("active");
+  } ) 
+    
+  );
